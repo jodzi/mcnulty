5be0e9a7e5ab6b97709e29c9f6f92094ded37dd1
= <=>
2015-05-06 22:47:08 -0400

    updated class

--- Classifier_Class.py
+++ Classifier_Class.py
@@ -12,32 +12,51 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree #import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.metrics import roc_curve, accuracy_score, precision_score, recall_score, f1_score, auc, roc_curve
 from sklearn.learning_curve import learning_curve
+from sklearn import preprocessing
+from sklearn.externals.six import StringIO  
+import pydot
 
-bank = pd.read_csv('bank.csv', sep=';') 
-bank = bank.replace(to_replace=['yes','no'], value=[1,0])
+### Look into gradient boosting methods
 
+## Setup dataframe 
+bank_add = pd.read_csv('bank-additional.csv', sep=';') 
+bank_add = bank_add.replace(to_replace=['yes','no'], value=[1,0])
+#bank_add['cons.price.idx'] = preprocessing.scale(bank_add['cons.price.idx'])
+bank_add_dum = pd.get_dummies(bank_add, columns=['loan', 'job', 'marital', 'education','default', 'housing', 'contact', 'poutcome'])
+
+# Features = All columns
+xcols = [0,4,5,6,7,8,9,10,11]
+xcols.extend(range(13, bank_add_dum.shape[1]))
+ycols = bank_add_dum.columns.get_loc('y')
+
+## Data Exploration
+#bank_dum.groupby('y').mean()
+#plt.scatter(bank['scaled_balance'][bank['y'] == 1], bank['age'][bank['y'] == 1], c='r')
+#plt.scatter(bank['scaled_balance'][bank['y'] == 0], bank['age'][bank['y'] == 0], c='y', alpha=0.1)
 
 class Classifier(object):
     """
-    builds a classifier object using an estimator model, an X array of features
-    and a y response array.
+    builds a classifier object using an estimator model, a pandas dataframe, xcols 
+    of features, and ycols for response label
     model = estimator
-    X = numpy array
-    y = numpy array
+    dataframe = pandas dataframe
+    xcols = tuple of integers
+    ycols = integer
     """
     
-    def __init__(self, model, dataframe, xcols, ycols): # X, y):
+    def __init__(self, model, dataframe, xcols, ycols):
         self.model = model
         self.xcols = xcols
         self.ycols = ycols
         self.score_metrics = ['accuracy', 'precision', 'recall', 'f1']
-        self.X = dataframe.values[:, self.xcols].astype(int)
-        self.y = dataframe.values[:, self.ycols].astype(int)
+        self.X = dataframe.values[:, self.xcols].astype(float)
+        self.y = dataframe.values[:, self.ycols].astype(float)
         self.train, self.test = train_test_split(dataframe)
 
         
@@ -59,14 +78,16 @@ class Classifier(object):
         """
         returns training set
         """
-        return self.train[:, self.xcols].astype(int), self.train[:, self.ycols].astype(int)
+        return self.train.values[:, self.xcols].astype(float), \
+                self.train.values[:, self.ycols].astype(float)
         
          
     def get_test(self):
         """
         returns test set
         """
-        return self.test[:, self.xcols].astype(int), self.test[:, self.ycols].astype(int)
+        return self.test.values[:, self.xcols].astype(float), \
+                self.test.values[:, self.ycols].astype(float)
         
     
     def get_model(self):
@@ -88,8 +109,8 @@ class Classifier(object):
         returns a fit of the estimator on the training set
         """
         return self.model.fit(self.get_train()[0], self.get_train()[1])
-       
-       
+        
+         
     def get_predictions(self):
         """
         return predictions and prediction probabilities based off of the test set
@@ -117,7 +138,7 @@ class Classifier(object):
         if cv:
             return [np.mean(cross_val_score(self.get_model(), self.get_X(), self.get_y(), scoring=score)) \
                 for score in self.score_metrics]
-                    
+                              
     
     def print_metrics(self, cv=False, tt=False):
         """
@@ -161,4 +182,30 @@ class Classifier(object):
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
         plt.title('ROC Curve')
-
+        
+        
+def find_best_k(df, xcols, ycols):
+    
+    scores = []
+    
+    train, test = train_test_split(df)
+    
+    for i in range(1,21):
+        knn = KNeighborsClassifier(n_neighbors=i)
+        knn = knn.fit(train.values[:, xcols].astype(int), train.values[:, ycols].astype(int))
+        scores.append(knn.score(test.values[:, xcols].astype(int), test.values[:, ycols].astype(int))) 
+    
+    return scores.index(max(scores)) + 1
+    
+logistic = Classifier(LogisticRegression(), bank_add_dum, xcols, ycols)
+svm = Classifier(SVC(probability=True), bank_add_dum, xcols, ycols)
+knn = Classifier(KNeighborsClassifier(n_neighbors=find_best_k(bank_add_dum, xcols, ycols)), bank_add_dum, xcols, ycols)
+gnb = Classifier(GaussianNB(), bank_add_dum, xcols, ycols)
+dt = Classifier(tree.DecisionTreeClassifier(), bank_add_dum, xcols, ycols)
+forest = Classifier(RandomForestClassifier(), bank_add_dum, xcols, ycols)
+gradiant = Classifier(GradientBoostingClassifier(), bank_add_dum, xcols, ycols)
+     
+#dot_data = StringIO() 
+#tree.export_graphviz(dt.build_full_data_model(), out_file=dot_data) 
+#graph = pydot.graph_from_dot_data(dot_data.getvalue()) 
+#graph.write_pdf("bank.pdf")
